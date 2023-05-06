105f28a79e634da2275cf4c571cdfe1816588051
JaPeterson1 <james.peterson.ks@gmail.com>
2023-05-06 12:43:09 -0500

    Updated clearDatabase script to empty the table.

--- clearDatabase.py
+++ clearDatabase.py
@@ -2,6 +2,6 @@ import constants
 from sql import sqlConnection
 
 DB = sqlConnection(constants.SQL_HOST_IP, constants.SQL_USER_NAME, constants.SQL_PASSWORD)
-DB.runCommand("DROP TABLE datapoints")
+DB.runCommand("DELTE * FROM datapoints")
 
 print("Successfully cleared database.")
