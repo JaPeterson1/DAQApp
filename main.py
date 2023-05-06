ffa4347c7d03d0a013d5ff7569ce1c243b6f131c
JaPeterson1 <james.peterson.ks@gmail.com>
2023-05-06 12:12:09 -0500

    removed import*

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from solarchargerSim import solarcharger
 from windTurbine1 import windTurbine
 from apscheduler.schedulers.blocking import BlockingScheduler
 from sql import sqlConnection
-from constants import *
+import constants
 import time
 
 
-solarCharger1 = solarcharger(SOLAR_CHARGER_1_PORT)
-solarCharger2 = solarcharger(SOLAR_CHARGER_2_PORT)
-windTurbine1 = windTurbine(WIND_TURBINE_1_RADIUS_METERS, WIND_TURBINE_1_EFFICIENCY, WIND_TURBINE_1_AIR_DENSITY)
-windTurbine2 = windTurbine(WIND_TURBINE_2_RADIUS_METERS, WIND_TURBINE_2_EFFICIENCY, WIND_TURBINE_2_AIR_DENSITY)
-arduino = arduinoHelper(ARDUINO_PORT)
+solarCharger1 = solarcharger(constants.SOLAR_CHARGER_1_PORT)
+solarCharger2 = solarcharger(constants.SOLAR_CHARGER_2_PORT)
+windTurbine1 = windTurbine(constants.WIND_TURBINE_1_RADIUS_METERS, constants.WIND_TURBINE_1_EFFICIENCY, constants.WIND_TURBINE_1_AIR_DENSITY)
+windTurbine2 = windTurbine(constants.WIND_TURBINE_2_RADIUS_METERS, constants.WIND_TURBINE_2_EFFICIENCY, constants.WIND_TURBINE_2_AIR_DENSITY)
+arduino = arduinoHelper(constants.ARDUINO_PORT)
 print("Connecting...")
 time.sleep(5)
 
-DB = sqlConnection(SQL_HOST_IP, SQL_USER_NAME, SQL_PASSWORD)
+DB = sqlConnection(constants.SQL_HOST_IP, constants.SQL_USER_NAME, constants.SQL_PASSWORD)
 
 def updateDB(database):
     """Attempts to send the latest data for all sensors to the server until it succeeds. Should be run periodically.
@@ -36,13 +36,13 @@ def updateDB(database):
         except Exception as e:
             print("Failed to send, attempting reconnection...", "Explanation: ", e)
             try:
-                database = sqlConnection(SQL_HOST_IP, SQL_USER_NAME, SQL_PASSWORD)
+                database = sqlConnection(constants.SQL_HOST_IP, constants.SQL_USER_NAME, constants.SQL_PASSWORD)
             except:
                 print("Failed to reconnect, retrying...", "Explanation: ", e)
                 continue
 
 
 scheduler = BlockingScheduler()
-scheduler.add_job(updateDB, 'interval', seconds=serverUpdateFrequency, args=(DB,))
+scheduler.add_job(updateDB, 'interval', seconds=constants.serverUpdateFrequency, args=(DB,))
 scheduler.start()
 print("Exiting")
